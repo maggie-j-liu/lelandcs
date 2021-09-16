22c350a622c9a4af612ba3a8470afc2ed4b87469
maggie-j-liu <maggie.j.liu@gmail.com>
2021-09-15 17:38:27 -0700

    fix site url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_URL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:3000"
-    : "https://lelandcs.vercel.app";
+    ? "https://lelandcs.vercel.app"
+    : "http://localhost:3000";

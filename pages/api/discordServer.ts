77a7d51be834c4b2df2527810b3ab205974bbdbf
maggie-j-liu <maggie.j.liu@gmail.com>
2021-09-17 10:39:01 -0700

    start discord server api route

---
+++ pages/api/discordServer.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db, auth } from "@/utils/admin";
+// @ts-ignore
+import encoder from "firebase-key-encode";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method != "POST") {
+    res.redirect(303, "/404");
+    return;
+  }
+  console.log(req.body);
+  /*
+  const email = req.body["Email"].trim();
+  const encodedEmail = encoder.encode(email);
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Basic ")) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  
+  const credentials = authHeader.split(" ")[1];
+  const decodedCredentials = Buffer.from(credentials, "base64").toString();
+  if (
+    decodedCredentials !==
+    process.env.GOOGLE_FORM_USERNAME + ":" + process.env.GOOGLE_FORM_PASSWORD
+  ) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  let user = null;
+  try {
+    user = await auth.getUserByEmail(email);
+  } catch (e) {
+    console.log(e);
+    console.log("no user");
+  }
+  if (user === null) {
+    await db.ref("googleFormSubmissions").update({
+      [encodedEmail]: true,
+    });
+  } else {
+    console.log(user.uid);
+    console.log(encodedEmail);
+    await db.ref().update({
+      [`googleFormSubmissions/${encodedEmail}`]: true,
+      [`users/${user.uid}/formSubmitted`]: true,
+    });
+  }
+  */
+  res.status(200).send("Success");
+}

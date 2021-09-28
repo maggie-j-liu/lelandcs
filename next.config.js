edc16e9bc7be77d40f8d3c86effc340bdf2c9862
maggie-j-liu <maggie.j.liu@gmail.com>
2021-09-27 22:20:26 -0700

    short urls for important links

--- next.config.js
+++ next.config.js
@@ -10,6 +10,29 @@ module.exports = {
         destination: "/tickets",
         permanent: true,
       },
+      {
+        source: "/discord",
+        destination: "https://discord.gg/bVxW6B9yDt",
+        permanent: true,
+      },
+      {
+        source: "/signup",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSeo53lNcDbaR9tEb8P7hiH1WTYu0zPlnyyMZ-Sya3GSnMREuA/viewform",
+        permanent: true,
+      },
+      {
+        source: "/officer",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSeaCe9MAnozIkKheEfEfA-0-7-brtHNa0t3_T07pdMF9a44uw/viewform",
+        permanent: true,
+      },
+      {
+        source: "/feedback",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSffNB1I2vv-ZaeB-MPPU_l34Z9FeaOq3U1POmpPfIp-5_ciDg/viewform",
+        permanent: true,
+      },
     ];
   },
 };

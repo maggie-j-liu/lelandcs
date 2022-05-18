42ef44d0baa513b22e76ce5a5a24031e6d893bdb
maggie-j-liu <maggie.j.liu@gmail.com>
2022-05-18 11:46:42 -0700

    bump tailwindcss version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     screens: {
       xs: "475px",
